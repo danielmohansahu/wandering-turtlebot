4778c5357dd8fcd870e2173aac1b46d4ed8fd984
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-11-23 18:59:48 -0500

    Added stub implementation of core Walker class

--- include/wandering_turtlebot/walker.h
+++ include/wandering_turtlebot/walker.h
@@ -1,6 +1,38 @@
 #pragma once
 
+#include <atomic>
+
+#include <ros/ros.h>
+
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
+
 namespace walker {
 
+class Walker {
+ public:
+  /* @brief Constructor */
+  Walker();
+
+  /* @brief Core execution method; this should be called regularly by the external thread. */
+  void execute();
+
+ private:
+
+  /* @brief Publish a forward commanded velocity to continue exploring. */
+  void forward();
+
+  /* @brief Spin in place to find a safe route. */
+  void spin();
+
+  /* @brief Boolean indicating whether or not we're almost in collision. */
+  std::atomic<bool> near_obstacle_;
+
+  /* @brief Core ROS subscriber to laser sensor for collision detection. */
+  ros::Subscriber laser_sub_;
+
+  /* @brief Core ROS publisher to set command velocity. */
+  ros::Subscriber cmd_pub_;
+};
 
 } //namespace walker
