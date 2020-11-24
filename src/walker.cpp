31107cfe2d8ea4bbbf4bbf68830a69500550a07d
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-11-23 19:00:02 -0500

    Stub implementation of expected API

--- src/walker.cpp
+++ src/walker.cpp
@@ -1,1 +1,25 @@
 #include <wandering_turtlebot/walker.h>
+
+namespace walker {
+
+Walker::Walker() : near_obstacle_(false) {
+  // initialize ROS communication objects
+
+  // ros::Subscriber laser_sub_;
+  // ros::Subscriber cmd_pub_;
+
+}
+
+void Walker::execute() {
+
+}
+
+void Walker::forward() {
+
+}
+
+void Walker::spin() {
+
+}
+
+} //namespace walker
